2d998756f8ab110b1f762edde2b494f31bcd32e3
Kyoorim <kyoorim88@gmail.com>
2022-12-11 22:59:47 +0900

    로그인 api기능구현

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
 const apiRoot = axios.create({
 	baseURL: 'https://pre-onboarding-selection-task.shop/',
@@ -16,6 +17,22 @@ export const SignUp = async (form) => {
 	}
 };
 
+export const SignIn = async (form) => {
+	try {
+		const response = await axios.post('/auth/signin', form, {
+			withCredentials: true,
+		});
+		const token = response.data.access_token;
+		const decodedUserInfo = jwt_decode(token);
+		localStorage.setItem('userInfo', JSON.stringify(decodedUserInfo));
+		console.log(response);
+		return response;
+	} catch (err) {
+		alert(err);
+	}
+};
+
 export const apiService = {
 	SignUp,
+	SignIn,
 };
--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,24 +1,45 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { apiService } from '../../apis';
 import * as S from './style';
 
 const Signin = () => {
+	const navigate = useNavigate();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const submitHandler = async (event) => {
+		event.preventDefault();
+
+		const form = {
+			email,
+			password,
+		};
+
+		const response = await apiService.SignIn(form);
+		if (response.status === 200) {
+			alert('로그인 성공');
+			navigate('/');
+		}
+	};
 	return (
 		<S.Wrapper>
 			<h1>투두두두</h1>
-			<S.Form>
+			<S.Form onSubmit={submitHandler}>
 				<S.Label htmlFor="email">Email</S.Label>
 				<S.Input
-				//   type="email"
-				//   id="email"
-				//   value={email}
-				//   onChange={(e) => setEmail(e.target.value)}
+					type="email"
+					id="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
 				></S.Input>
 				<S.Label htmlFor="password">Password</S.Label>
 				<S.Input
-				//   type="password"
-				//   id="password"
-				//   required
-				//   value={password}
-				//   onChange={(e) => setPassword(e.target.value)}
+					type="password"
+					id="password"
+					required
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
 				></S.Input>
 				<S.ButtonContainer>
 					<button>로그인하기</button>
